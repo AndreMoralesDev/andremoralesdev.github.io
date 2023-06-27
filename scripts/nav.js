ddc918d9b57b11c11df4e0f9a95de9865ec57205
Matias Morales <matias.plin@gmail.com>
2023-06-26 20:38:54 -0500

    Menú espía arreglado, ahora es completamente responsive

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,9 +1,28 @@
 const  d = document;
 const nav = (dots, button, navOptions) => {
     const $dots = d.querySelectorAll(dots);
-    const $sections = d.querySelectorAll(".section-container")
+    const $sections = d.querySelectorAll(".section-container");
     const $btn = d.getElementById(button);
     const $navOptions = d.querySelector(navOptions);
+    const $header = d.querySelector("header.nav-container");
+
+    d.addEventListener("scroll", () => {
+        $sections.forEach(section => {
+            const marginTop = $header.getBoundingClientRect().height;
+            const top = window.scrollY;
+            const height = section.offsetHeight;
+            const offset = section.offsetTop - marginTop * 1.5;
+            const id = parseInt(section.getAttribute("data-index"));
+    
+            if (top >= offset && top < offset + height) {
+                $dots.forEach((dot, index) => {
+                    if (index === id) dot.classList.add("active");
+                    else dot.classList.remove("active");
+                })
+            }
+        })
+    })
+
     d.addEventListener("click", e => {
         if (e.target === $btn) {
             $navOptions.classList.toggle("active");
@@ -11,15 +30,6 @@ const nav = (dots, button, navOptions) => {
             $btn.lastElementChild.classList.toggle("display-none");
         }
     })
-    const cb = entries => {
-        entries.forEach(entry => {
-            const id = parseInt(entry.target.getAttribute("data-index"));
-            if (entry.isIntersecting) $dots[id].classList.add("active");
-            else $dots[id].classList.remove("active");
-        })
-    }
-    const observer = new IntersectionObserver(cb, {threshold: 0.50001});
-    $sections.forEach(section => observer.observe(section));
 }
 
 export default nav;
